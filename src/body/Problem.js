98bd55a7eebcba7fa23df09a5c772a2ce7fe22f1
Andreas Wassum <a.wassum@gmail.com>
2020-06-14 18:18:19 +0200

    Minor updates

--- src/body/Problem.js
+++ src/body/Problem.js
@@ -1,17 +1,20 @@
 import React from "react";
-import codeforcesIcon from "../images/codeforces.png"
+import codeforcesIcon from "../images/codeforces.png";
+import greenCheckMark from "../images/green-check-mark.png";
 import styled from "styled-components";
 
 const ProblemRow = styled.div`
   display: flex;
   flex-flow: row nowrap;
+  align-items: center;
 `;
 
 const Problem = ({problem}) => {
     return(
         <ProblemRow>
             <img src={codeforcesIcon} />
-            <p>{problem.problemName} </p>
+            <a href={problem.problemUrl} target="_blank">{problem.problemName}</a>
+            <img src={greenCheckMark} />
         </ProblemRow>)
 }
 
